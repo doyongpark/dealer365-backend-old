bdffc627187302f06a726f8ae621334d9b796739
Doyong Park <doyong.park@inventis.co.kr>
2024-10-08 10:12:26 -0400

    reset

--- libs/test1/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/test1.module';
-export function GetTest1Data() {return 'Test 1 Data'}
--- libs/test2/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/test2.module';
-export function GetTest2Data() {return 'Test 2 Data'}

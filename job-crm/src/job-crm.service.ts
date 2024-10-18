6fe19a5d6cd8e8d80a8dfc1b69cbbd848b833509
Doyong Park <doyong.park@inventis.co.kr>
2024-10-18 15:59:31 -0400

    add job again

---
+++ job-crm/src/job-crm.service.ts
@@ -0,0 +1,30 @@
+import { ServiceBusReceivedMessage } from '@azure/service-bus';
+import { IRepository } from '@dealer365-backend/database';
+import { IBrokerService } from '@dealer365-backend/message-broker';
+import { LeadDto } from '@dealer365-backend/package-crm';
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class JobCrmService implements OnModuleInit, OnModuleDestroy {
+
+  constructor(
+    private readonly brokerService: IBrokerService,
+    private readonly repository: IRepository<LeadDto>
+  ) { }
+
+  async onModuleInit() {
+    this.brokerService.receiveMessage(this.processMessage.bind(this));
+  }
+
+  async onModuleDestroy() {
+    await this.brokerService.closeReceiver();
+    await this.brokerService.closeClient();
+  }
+
+  private async processMessage(message: ServiceBusReceivedMessage) {
+    Logger.debug(`Message processing in job-crm, message: ${JSON.stringify(message.body)}`);
+    // Add your message processing logic here
+    const result = await this.repository.findAll();
+    Logger.debug(`Result from repository: ${JSON.stringify(result)}`);
+  }
+}

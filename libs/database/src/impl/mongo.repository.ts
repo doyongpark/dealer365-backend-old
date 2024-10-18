668a10b97fe696166db5322d596756e43dd6f732
Doyong Park <doyong.park@inventis.co.kr>
2024-10-18 12:51:43 -0400

    add env.

--- libs/database/src/impl/mongo.repository.ts
+++ libs/database/src/impl/mongo.repository.ts
@@ -11,38 +11,40 @@ export class IMongoRepository<T> extends IRepository<T> {
   }
 
   async create(item: T): Promise<T> {
-    const document = convertToDocument(this.model, item);
+    const document = this.convertToDocument(this.model, item);
     const createdDocument = await document.save();
-    return convertToPlainObject<T>(createdDocument);
+    return this.convertToPlainObject<T>(createdDocument);
   }
 
   async findAll(): Promise<T[]> {
     const documents = await this.model.find().exec();
-    return documents.map(doc => convertToPlainObject<T>(doc));
+    return documents.map(doc => this.convertToPlainObject<T>(doc));
   }
 
   async findOne(id: string): Promise<T> {
     const document = await this.model.findById(id).exec();
-    return convertToPlainObject<T>(document);
+    return this.convertToPlainObject<T>(document);
   }
 
   async update(id: string, item: UpdateQuery<T>): Promise<T> {
     const document = await this.model.findByIdAndUpdate(id, item, { new: true }).exec();
-    return convertToPlainObject<T>(document);
+    return this.convertToPlainObject<T>(document);
   }
 
   async delete(id: string): Promise<void> {
     await this.model.findByIdAndDelete(id).exec();
   }
-}
 
-// 변환 함수들
-function convertToDocument<T>(model: Model<Document>, plainObject: T): Document {
-  return new model(plainObject);
-}
 
-function convertToPlainObject<T>(document: Document): T {
-  const plainObject = document.toObject();
-  delete plainObject.__v;
-  return plainObject as T;
+
+  // 변환 함수들
+  convertToDocument<T>(model: Model<Document>, plainObject: T): Document {
+    return new model(plainObject);
+  }
+
+  convertToPlainObject<T>(document: Document): T {
+    const plainObject = document.toObject();
+    delete plainObject.__v;
+    return plainObject as T;
+  }
 }

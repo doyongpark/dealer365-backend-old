9b3fa71cfcf57c7edf7a6300d17db5d7e7cdb44a
Doyong Park <doyong.park@inventis.co.kr>
2024-10-20 15:31:22 -0400

    dd

--- apps/job-crm/src/main.ts
+++ apps/job-crm/src/main.ts
@@ -1,10 +1,7 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { JobCrmModule } from './job-crm.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(JobCrmModule);
-  Logger.debug('Nest application started');
+  await NestFactory.createApplicationContext(JobCrmModule);
 }
-
 bootstrap();
